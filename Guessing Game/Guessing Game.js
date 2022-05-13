e3afb4cbb2283a0738666bb7ad10a831c53e42fd
Jeffrey Chi <thejeffreychi@gmail.com>
2022-05-13 02:14:20 -0700

    Update Guessing Game.js

--- Guessing Game/Guessing Game.js
+++ Guessing Game/Guessing Game.js
@@ -2,7 +2,7 @@
 let maximum = parseInt(prompt('Enter the maximum number!'));
 while (!maximum) {
     maximum = parseInt(prompt('Enter a valid number!'));
-}
+} 
 
 let targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum);
